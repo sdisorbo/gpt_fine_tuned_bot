45fe8001c683b057a601a67521f851af891a32ec
Sam DiSorbo <134333812+sdisorbo@users.noreply.github.com>
2023-07-06 23:54:02 -0400

    Create prompt_tweet_completion.py

---
+++ prompt_tweet_completion.py
@@ -0,0 +1,5 @@
+openai.Completion.create(
+    model='ada:ft-personal-2023-06-28-03-11-02', #2nd mod
+    #model='ada:ft-personal-2023-06-30-17-30-38', #3rd mod
+    prompt="DeAndre Hopkins ->",
+    max_tokens=60)
